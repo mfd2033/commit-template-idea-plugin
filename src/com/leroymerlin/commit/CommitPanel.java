17ee3ae738ad241f74e4a5700864dd4800519043
马富荻 <mafudi@digibird.com.cn>
2019-12-22 01:52:36 +0800

    feat: scope的显示内容基于当前用户的提交记录过滤。

--- src/com/leroymerlin/commit/CommitPanel.java
+++ src/com/leroymerlin/commit/CommitPanel.java
@@ -5,11 +5,15 @@ import com.intellij.openapi.vfs.VfsUtil;
 
 import javax.swing.*;
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Damien Arrachequesne
  */
 public class CommitPanel {
+
+    private static final Logger logger = Logger.getLogger(CustomConfigurable.class.getName());
     private JPanel mainPanel;
     private JComboBox changeType;
     private JComboBox changeScope;
@@ -23,7 +27,14 @@ public class CommitPanel {
             changeType.addItem(type);
         }
         File workingDirectory = VfsUtil.virtualToIoFile(project.getBaseDir());
-        Command.Result result = new Command(workingDirectory, "git log --all --format=%s | grep -Eo '^[a-z]+(\\(.*\\)):.*$' | sed 's/^.*(\\(.*\\)):.*$/\\1/' | sort -n | uniq").execute();
+        Command.Result gitUserEmailGetCmdResult = new Command(workingDirectory, "git config user.email").execute();
+        if (!gitUserEmailGetCmdResult.isSuccess()) {
+            return;
+        }
+        String gitUserEmail = gitUserEmailGetCmdResult.getOutput().get(0);
+        logger.log(Level.INFO, "Git用户邮箱：" + gitUserEmail);
+        String gitLogCmd = "git log --author=" + gitUserEmail + " --format=%s | grep -Eo '^[a-z]+(\\(.*\\)):.*$' | sed 's/^.*(\\(.*\\)):.*$/\\1/' | sort -n | uniq";
+        Command.Result result = new Command(workingDirectory, gitLogCmd).execute();
         if (result.isSuccess()) {
             result.getOutput().forEach(changeScope::addItem);
         }

7740e6a7a01cc145c7f8c6584a7e6d8b5fc1953f
马富荻 <mafudi@digibird.com.cn>
2021-02-06 11:25:09 +0800

    refactor: “类型”修改为单选框。

--- src/com/leroymerlin/commit/CommitPanel.java
+++ src/com/leroymerlin/commit/CommitPanel.java
@@ -5,9 +5,9 @@ import com.intellij.openapi.vfs.VfsUtil;
 import org.apache.commons.lang.StringUtils;
 
 import javax.swing.*;
-import java.awt.event.ItemEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.Objects;
+import java.util.Enumeration;
 import java.util.logging.Logger;
 
 /**
@@ -18,18 +18,24 @@ public class CommitPanel {
     private static final Logger logger = Logger.getLogger(CustomConfigurable.class.getName());
     private static final String LONG_DESCRIPTION_DEFAULT_CONTENT = "问题原因：\n解决方案：";
     private JPanel mainPanel;
-    private JComboBox changeType;
     private JComboBox changeScope;
     private JTextField shortDescription;
     private JTextArea longDescription;
     private JTextArea sourceKeyword;
     private JTextArea breakingChanges;
     private JLabel breakingChangesHelp;
+    private JRadioButton featRadioButton;
+    private JRadioButton fixRadioButton;
+    private JRadioButton docsRadioButton;
+    private JRadioButton styleRadioButton;
+    private JRadioButton refactorRadioButton;
+    private JRadioButton perfRadioButton;
+    private JRadioButton testRadioButton;
+    private JRadioButton buildRadioButton;
+    private JRadioButton choreRadioButton;
+    private ButtonGroup changeTypeGroup;
 
     CommitPanel(Project project) {
-        for (ChangeType type : ChangeType.values()) {
-            changeType.addItem(type);
-        }
         File workingDirectory = VfsUtil.virtualToIoFile(project.getBaseDir());
         Command.Result gitUserEmailGetCmdResult = new Command(workingDirectory, "git config user.email").execute();
         if (!gitUserEmailGetCmdResult.isSuccess()) {
@@ -42,20 +48,31 @@ public class CommitPanel {
         if (result.isSuccess()) {
             result.getOutput().forEach(changeScope::addItem);
         }
-        changeType.addItemListener(itemEvent -> {
-            if (Objects.equals(itemEvent.getStateChange(), ItemEvent.SELECTED)) {
-                ChangeType s = (ChangeType) itemEvent.getItem();
-                if (StringUtils.isEmpty(longDescription.getText()) || LONG_DESCRIPTION_DEFAULT_CONTENT.equals(longDescription.getText())) {
-                    if (ChangeType.FIX.equals(s)) {
-                        longDescription.setText(LONG_DESCRIPTION_DEFAULT_CONTENT);
-                    } else {
-                        longDescription.setText("");
-                    }
-                }
-            }
-        });
         breakingChangesHelp.setToolTipText("重大更改，当前代码与上一个版本不兼容。" +
                 "<br>比如：<br>基础业务模型变更<br>公共模块的接口变更<br>添加过时标记（@Deprecated）");
+        initRadioButtonActionListener();
+    }
+
+    private void initRadioButtonActionListener() {
+        featRadioButton.addActionListener(getCommonRadioButtonActionListener(""));
+        fixRadioButton.addActionListener(getCommonRadioButtonActionListener(LONG_DESCRIPTION_DEFAULT_CONTENT));
+        docsRadioButton.addActionListener(getCommonRadioButtonActionListener(""));
+        styleRadioButton.addActionListener(getCommonRadioButtonActionListener(""));
+        refactorRadioButton.addActionListener(getCommonRadioButtonActionListener(""));
+        perfRadioButton.addActionListener(getCommonRadioButtonActionListener(""));
+        testRadioButton.addActionListener(getCommonRadioButtonActionListener(""));
+        perfRadioButton.addActionListener(getCommonRadioButtonActionListener(""));
+        buildRadioButton.addActionListener(getCommonRadioButtonActionListener(""));
+        choreRadioButton.addActionListener(getCommonRadioButtonActionListener(""));
+    }
+
+    private ActionListener getCommonRadioButtonActionListener(final String description) {
+        return e -> {
+            if (!StringUtils.isEmpty(longDescription.getText()) && !LONG_DESCRIPTION_DEFAULT_CONTENT.equals(longDescription.getText())) {
+                return;
+            }
+            longDescription.setText(description);
+        };
     }
 
     JPanel getMainPanel() {
@@ -64,13 +81,23 @@ public class CommitPanel {
 
     CommitMessage getCommitMessage() {
         return new CommitMessage(
-                (ChangeType) changeType.getSelectedItem(),
+                getSelectedChangeType(),
                 (String) changeScope.getSelectedItem(),
                 shortDescription.getText().trim(),
                 longDescription.getText().trim(),
                 sourceKeyword.getText().trim(),
                 breakingChanges.getText().trim()
         );
+    }
+
+    private ChangeType getSelectedChangeType() {
+        for (Enumeration<AbstractButton> buttons = changeTypeGroup.getElements(); buttons.hasMoreElements();) {
+            AbstractButton button = buttons.nextElement();
+            if (button.isSelected()) {
+                return ChangeType.valueOf(button.getActionCommand().toUpperCase());
+            }
+        }
+        return null;
     }
 
 }
